693409f7907556609e7144f78ed56ef9116e1ab7
loolys <pierre.liljekvist@outlook.com>
2017-01-25 19:22:59 +0100

    added instructions for api usage on homepage, added link to actual page of image aswell as direct link

--- index.js
+++ index.js
@@ -26,7 +26,12 @@ app.get('/search/:term', function(req, res) {
 		console.log('saved in db');
 	});
 	let call = req.params.term + '/time/0.json';
+	console.log(req.query.offset);
 	let offset = parseInt(req.query.offset);
+	if (isNaN(offset)) {
+		// default to 10 if no value given
+		offset = 10;
+	}
 	if (offset <= 0) {
 		offset = 1;
 	} else if (offset > 59) {
@@ -37,15 +42,44 @@ app.get('/search/:term', function(req, res) {
 		.then(function (info) {
 			let data = [];
 			for (let i = 0; i < offset; i++) {
+				let test = info.data.data[i].link.split('.').slice(0,3).join('.');
 				let imgData = {
 					link: info.data.data[i].link,
-					title: info.data.data[i].title
+					title: info.data.data[i].title,
+					pageLink: test
 				};
 				data.push(imgData);
 			}
 			res.setHeader('Content-Type', 'application/json');
 			res.send(JSON.stringify(data, null, 3));
 		});
+});
+
+app.get('/latest', function(req, res) {
+	// makes a query sorted by how the records are saved on disk
+	let searchData = [];
+	const query = searchModel.find().sort({$natural: -1}).limit(10);
+	query.exec(function(err, docs) {
+		if (err) throw err;
+		let result = docs.map(function(x) {
+			return x.term;
+		});
+		console.log(result);
+		
+		for (let i = 0; i < result.length; i++) {
+			let jason = {
+				term: result[i]
+			};
+			searchData.push(jason);
+		}
+		console.log(searchData);
+		res.setHeader('Content-Type', 'application/json');
+		res.json(searchData);
+	});
+});
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 const port = process.env.PORT || 3000;
